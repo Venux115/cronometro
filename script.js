d71e0219761d3ff9fb7aad50d11e2148c1ea0d4b
vinicius <goulart.vag@outlook.com>
2024-07-04 21:22:20 -0300

    Ajustes de qualidade de código.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const btn_inicar = document.querySelector(".button-start")
+const btn_iniciar = document.querySelector(".button-start")
 const btn_parar = document.querySelector(".button-stop")
 const btn_reiniciar = document.querySelector(".button-reload")
 const textoTempo = document.querySelector('p')
@@ -11,11 +11,9 @@ let horas = 0
 
 function iniciar(){
     parar()
-    
     cron = setInterval(contar, 1000)
 }
 function parar(){
-    
     clearInterval(cron)
 }
 
@@ -30,7 +28,6 @@ function reiniciar(){
 }
 
 function contar(){
-   
     segundos++
     if(segundos > 59){
         minutos++
@@ -40,42 +37,23 @@ function contar(){
         horas++
         minutos = 0
     }
-    
+
     mostrarTempo()
-
 }
 
-function intercalarBotoes(opcao){
-    if(opcao === 1){
-       
-        
-    
-       
-    }else if(opcao === 2)
-    {
-        btn_inicar.classList.toggle("hidden")
-        btn_parar.classList.toggle("hidden")
-    }else if(opcao === 3){
-        btn_inicar.classList.toggle("hidden")
-    }
-    
-}
 
 function mostrarTempo()
 {
-    
-    
-    
     const minutosFormatados = minutos < 10 ? '0' + minutos : minutos;
     const segundosFormatados = segundos < 10 ? '0' + segundos : segundos
     const horasFormatadas = horas <10 ? '0' + horas : horas
 
-    const tempoString = horas > 0 ?`${horasFormatadas}:${minutosFormatados}.${segundosFormatados}` :`${minutosFormatados}:${segundosFormatados}`
-    textoTempo.innerText = tempoString
+    textoTempo.innerText = horas > 0 ?`${horasFormatadas}:${minutosFormatados}.${segundosFormatados}` :`${minutosFormatados}:${segundosFormatados}`
+
 }
 
-btn_inicar.addEventListener('click', () =>{
-     btn_inicar.classList.toggle("hidden")
+btn_iniciar.addEventListener('click', () =>{
+     btn_iniciar.classList.toggle("hidden")
      btn_parar.classList.remove("hidden") 
      btn_reiniciar.classList.remove("hidden")
     iniciar()
@@ -83,14 +61,13 @@ btn_inicar.addEventListener('click', () =>{
 })
 
 btn_parar.addEventListener('click', () =>{
-    btn_inicar.classList.toggle("hidden")
+    btn_iniciar.classList.toggle("hidden")
     parar()
 })
 
 btn_reiniciar.addEventListener('click', () =>{
-    btn_inicar.classList.remove("hidden")
+    btn_iniciar.classList.remove("hidden")
     btn_parar.classList.add("hidden") 
     btn_reiniciar.classList.add("hidden")
     reiniciar()
-    
 })
